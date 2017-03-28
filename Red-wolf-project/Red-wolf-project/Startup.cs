113fd40ff53babf87617b70018d4dbbaaf31f3f0
Charlie Philpott <Philpott_001@hotmail.com>
2017-03-28 20:56:45 +0100

    Upload project

---
+++ Red-wolf-project/Red-wolf-project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Red_wolf_project.Startup))]
+namespace Red_wolf_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
